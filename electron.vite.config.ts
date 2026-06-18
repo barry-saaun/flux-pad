54ea4ec56b63cbf7f6ff57f0474fa79df831b482
Barry Saaun <panhaocr@gmail.com>
2026-06-18 17:12:52 +1000

    READY SET GO!

---
+++ electron.vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { defineConfig } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  main: {
+    build: {
+      sourcemap: true
+    }
+  },
+  preload: {},
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+    plugins: [react()]
+  }
+})
